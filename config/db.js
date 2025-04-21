125e7c9b5475ce2a93023043db56651b84d87048
haidooo124 <kenzoku271@gmail.com>
2025-04-21 12:46:04 +0700

    Update

--- config/db.js
+++ config/db.js
@@ -1,14 +1,24 @@
-const mysql = require('mysql2/promise');
+  const mysql = require('mysql2/promise');
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 20000
-});
-module.exports = db;
+  const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    connectTimeout: 20000
+  });
+(async () => {
+    try {
+      const conn = await pool.getConnection(); // test thử kết nối
+      console.log('✅ Kết nối DB thành công!');
+      conn.release();
+    } catch (err) {
+      console.error('❌ Lỗi kết nối DB:', err);
+    }
+  })();
+  
+  module.exports = pool;
